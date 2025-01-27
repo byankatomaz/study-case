91a0636b19f045bd4cacab9fc82acc0df3d48db6
mob7ca@bosch.com <Byanka.Monteiro@bosch.com>
2025-01-27 10:29:53 -0300

    ci: package and config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +9,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'primary-blue': '#106EB0',
+        'primary-orange': '#EC7000',
+        'primary-gray': '#33303E',
+        'second-gray': '#4E4B59',
+        'gray-phone': '#F4F4F4',
+        'txt-gray': '#7A7786',
+        'opacity-gray': 'rgba(100, 80, 57, 0.1)',
+      },
+      backgroundImage: {
+        'img_bg_hero': "url('../assets/bg-hero.jpg')",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
